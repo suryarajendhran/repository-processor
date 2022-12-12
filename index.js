98d8d96a1f53da4fb38f9cfaa42838b7460451ba
Surya Rajendhran <32726562+suryarajendhran@users.noreply.github.com>
2022-12-12 16:01:58 +0530

    feat: returns frequency map

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const https = require('https');
 const { Octokit } = require('octokit');
+const { map, groupBy, prop, length } = require('ramda');
 
 const octokit = new Octokit({
   auth: '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',
@@ -11,28 +12,11 @@ app.get('/commits', async (req, res) => {
   const repo = req.query.repo;
   const owner = req.query.owner;
 
-  const _commits = await octokit.paginate(octokit.rest.repos.listCommits, {
-    owner,
-    repo,
-  });
-
-  console.log(_commits.length);
-
-  // const { data: _commits } = await octokit.rest.repos.listCommits({
-  //   owner,
-  //   repo,
-  //   per_page: 500,
-  // })
-
-  const commits = await Promise.all(_commits.map((commit) => octokit.rest.repos.getCommit({
-    owner,
-    repo,
-    ref: commit.sha
-  })).map((promise) => promise.then((response) => response.data)));
+  const commits = await getCommits(owner, repo);
 
   console.log(commits.length);
 
-  const fixedCommits = commits.filter(commit => commit.commit.message.toLowerCase().includes('fix') && !commit.commit.message.toLowerCase().includes('dependabot'));
+  const fixedCommits = commits.filter(filterCommitsWithAFix);
 
   const files = fixedCommits.map(commit => {
     return {
@@ -41,37 +25,36 @@ app.get('/commits', async (req, res) => {
     }
   });
 
-  console.log(files.flatMap(item => item.fileNames.map(fileName => ({
+  console.log(files);
+
+  const fileNamesMap = files.flatMap(item => item.fileNames.map(fileName => ({
     message: item.message,
     fileName: fileName
-  }))));
+  })));
 
-  res.send(files);
+  const fixedFrequencyMap = map(length, groupBy(prop('fileName'), fileNamesMap));
+
+  res.send(fixedFrequencyMap);
 });
-
-function getFilesForCommit(repo, commit) {
-  return new Promise((resolve, reject) => {
-    const options = {
-      host: `api.github.com`,
-      path: `/repos/${repo}/commits/${commit.sha}`,
-      headers: { 'User-Agent': 'My-App-Name' }
-    };
-
-    https.get(options, response => {
-      let body = '';
-      response.on('data', data => {
-        body += data;
-      });
-
-      response.on('end', () => {
-        console.log('body: ', body);
-        const commit = JSON.parse(body);
-        resolve(commit.files);
-      });
-    });
-  });
-}
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
+
+async function getCommits(owner, repo) {
+  const _commits = await octokit.paginate(octokit.rest.repos.listCommits, {
+    owner,
+    repo,
+  });
+
+  return await Promise.all(_commits.map((commit) => octokit.rest.repos.getCommit({
+    owner,
+    repo,
+    ref: commit.sha
+  })).map((promise) => promise.then((response) => response.data)));
+}
+
+function filterCommitsWithAFix(commit) {
+  return commit.commit.message.toLowerCase().includes('fix')
+    && !commit.commit.message.toLowerCase().includes('dependabot');
+}
